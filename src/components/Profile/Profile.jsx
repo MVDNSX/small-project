afb1c821f2ac5d878caf9fcac96697c04bfad1c6
MVDNS <a.s.meleshenkov@gmail.com>
2023-06-27 23:28:33 +0300

    edit profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,43 @@
+import { useState } from 'react'
+import avatar from '../../assets/avatar/user-avatar.png'
 import './Profile.css'
 const Profile = () => {
+	const [userName, setUserName] = useState('Eren yeager');
+	const [userEmail, setUserEmail] = useState('Eren101@gmail.com')
 	return (
 		<div className="profile-container">
-			<div className="user-info">Lorem ipsum dolor sit amet.</div>
-			<div className="most-order">Lorem ipsum dolor sit amet.</div>
+
+			<div className="user-profile">
+
+				<div className="user-data">
+
+					<div className="user-data__avatar"><img src={avatar} alt="" /></div>
+
+					<div className="user-data__info">
+						<div className="user-data__name">Eren Yeager</div>
+						<div className="user-data__creation">Member since 12 mars 2023</div>
+					</div>
+
+				</div>
+
+				<form className="user-data__form">
+					<label htmlFor="udn" className='user-data__label' >Name</label>
+					<input id='udn' type="text" className='user-data__input' 
+					value={userName}/>
+					<label htmlFor="ude" className='user-data__label' >Email</label>
+					<input id='ude' type="text" className='user-data__input' 
+					value={userEmail}/>
+				</form>
+
+			</div>
+
+			<div className="most-order">
+
+				<div className="most-order__title">Most Ordered</div>
+
+			</div>
+
+
 			<div className="order-report">
 				<div className="order-report__header">
 					<div className="order-report__title">Eren Yeager’s orders</div>
